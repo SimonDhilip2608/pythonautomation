c085d6b60527d32863f9924d4cabe6c8ab0fd398
SimonDhilip2608 <44920200+SimonDhilip2608@users.noreply.github.com>
2025-04-08 11:13:36 -0500

    Delete init.py

--- init.py
+++
@@ -1,6 +0,0 @@
-# services/__init__.py
-from services.elk_service import ELKService
-from services.vegasgpt_service import AIService
-from services.synapt_service import SynaptService
-
-__all__ = ['ELKService','VegasGPTService','SynaptService']
