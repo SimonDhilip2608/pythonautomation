eb5c4f5117d682fc1d326f5a8c3fbec8c169015a
SimonDhilip2608 <44920200+SimonDhilip2608@users.noreply.github.com>
2025-04-07 22:38:25 -0500

    Update init.py

--- init.py
+++ init.py
@@ -1,5 +1,4 @@
 # services/__init__.py
-# Empty init file to make the directory a package
 from services.elk_service import ELKService
 from services.vegasgpt_service import AIService
 from services.synapt_service import SynaptService
